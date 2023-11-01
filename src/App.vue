47be869f76cba50297e210fbc8c50a4d9cb5b6e7
kleberaluizio <kleberaluizio@gmail.com>
2023-11-01 00:57:21 -0300

    Adding components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <main class="columns is-gapless is-multiline"> <!-- Várias colunas, sem espaçamento e multiplas linhas (BULMA)-->
-    <div class="column is-one-quater">
-
+    <div class="column is-one-quarter">
+      <SideBar />
     </div>
-    <div class="column is-three-quater">
-
+    <div class="column is-three-quarter">
+      <MyForm />
     </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue'
+import MyForm   from './components/MyForm.vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components:{
+    SideBar
+    ,
+    MyForm
+  }
 });
 </script>
 
 <style></style>
+
---
+++ src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1> <img src="../assets/logo.png" alt="" srcset=""></h1>
+    </header>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: 'SideBar'
+})
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background-color: #0d3b66;
+    width: 100vb;
+    height: 100vh;
+}
+
+img {
+    /* border-radius: 25px; */
+    /* margin: 5px; */
+    width: 30%;
+    height: 30%;
+}
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
