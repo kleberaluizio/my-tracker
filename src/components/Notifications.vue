35635a22dfda157cf2dd8cf92ad37f0011a689c1
kleberaluizio <kleberaluizio@gmail.com>
2023-11-17 18:13:33 -0300

    Adding message popup

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,20 +1,47 @@
 <template>
-<div class="notifications">
-    <article class="message is-success">
-        <div class="message-header">
-            
-        </div>
-    </article>
-</div>
+    <div class="notifications">
+        <article class="message is-success">
+            <div class="message-header">
+                Attention
+            </div>
+            <div class="message-body">
+                Here goes a notification text!
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">
+                Attention
+            </div>
+            <div class="message-body">
+                Here goes a notification text!
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                Attention
+            </div>
+            <div class="message-body">
+                Here goes a notification text!
+            </div>
+        </article>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent ({
+export default defineComponent({
 
     name: 'NotificationView',
 })
 </script>
 
-<style></style>
+<style scoped>
+.notifications{
+    position: absolute;
+    right: 0;
+    width: 300px;
+    padding: 8px;
+    z-index: 105;
+}
+</style>
